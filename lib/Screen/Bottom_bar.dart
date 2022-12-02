daa47370f6437c813fde17d6ab8aa722cedc4e48
sireyrath <soksireyrath18@kit.edu.kh>
2022-12-02 13:44:25 +0700

    added heading/icon/search_Bar to HomeScreen 2/dec

--- lib/Screen/Bottom_bar.dart
+++ lib/Screen/Bottom_bar.dart
@@ -1,6 +1,7 @@
 // ignore: file_names
 import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/material.dart';
+import 'package:ticket_booking/Screen/home_screen.dart';
 
 class BottomBar extends StatefulWidget {
   const BottomBar({super.key});
@@ -20,7 +21,7 @@ class _BottomBarState extends State<BottomBar> {
   }
 
   final List<Widget> widgetOption = <Widget>[
-    const Text("Home"),
+    const HomeScreen(),
     const Text("Search"),
     const Text("Ticket"),
     const Text("Profile"),
@@ -28,14 +29,11 @@ class _BottomBarState extends State<BottomBar> {
 
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // centerTitle: true,
-        title: const Text(" My Ticket "),
-      ),
       body: Center(
         child: widgetOption[selectedIndex],
       ),
       bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
         currentIndex: selectedIndex,
         onTap: onItemTapped,
         showSelectedLabels: false,
@@ -49,8 +47,9 @@ class _BottomBarState extends State<BottomBar> {
               activeIcon: Icon(FluentSystemIcons.ic_fluent_home_filled),
               label: "Home"),
           BottomNavigationBarItem(
-              icon: Icon(FluentSystemIcons.ic_fluent_search_regular),
-              activeIcon: Icon(FluentSystemIcons.ic_fluent_search_filled),
+              icon: Icon(FluentSystemIcons.ic_fluent_search_square_regular),
+              activeIcon:
+                  Icon(FluentSystemIcons.ic_fluent_search_square_filled),
               label: "Home"),
           BottomNavigationBarItem(
               icon: Icon(FluentSystemIcons.ic_fluent_airplane_regular),
---
+++ lib/Screen/home_screen.dart
@@ -0,0 +1,96 @@
+import 'dart:ui';
+
+import 'package:fluentui_icons/fluentui_icons.dart';
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:ticket_booking/Screen/app_stlye.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Styles.bgColor,
+      body: ListView(
+        children: [
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              children: [
+                const Gap(40),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Good Morning",
+                          style: Styles.headLineStyle3,
+                        ),
+                        const Gap(5),
+                        Text(
+                          "Booking Tickets",
+                          style: Styles.headLineStyle1,
+                        ),
+                      ],
+                    ),
+                    Container(
+                      width: 50,
+                      height: 50,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          image: const DecorationImage(
+                              fit: BoxFit.fitWidth,
+                              image: AssetImage("assets/images/airplane.png"))),
+                    )
+                  ],
+                ),
+                const Gap(25),
+                Container(
+                  //height: 45,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: const Color(0xFFF4F6FD),
+                  ),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 12, vertical: 12),
+                  child: Row(
+                    children: [
+                      Icon(FluentSystemIcons.ic_fluent_search_regular,
+                          color: Colors.orange.shade300),
+                      Text(
+                        "Search",
+                        style: Styles.headLineStyle4,
+                      ),
+                    ],
+                  ),
+                ),
+                const Gap(40),
+                Container(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Upcoming Flights",
+                        style: Styles.headLineStyle2,
+                      ),
+                      InkWell(
+                        child: Text(
+                          "View all",
+                          style: Styles.textStyle
+                              .copyWith(color: Styles.primaryColor),
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
